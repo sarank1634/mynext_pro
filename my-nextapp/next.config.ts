b38dbc50b7f25eb6faeaf0976abb7cfdd0ae06f8
sarank1634 <saravanakumar1634@gmail.com>
2026-03-15 17:34:46 +0530

    28.26 image url has been comleted

--- my-nextapp/next.config.ts
+++ my-nextapp/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [{ hostname: "upload.wikimedia.org" }],
+  }
 };
 
 export default nextConfig;
--- my-nextapp/src/app/layout.tsx
+++ my-nextapp/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,9 +28,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div>Navbar</div>
-        {children}
-      </body>
-    </html>
+        <div>
+          <Link href="/home">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/contact">Contact</Link>
+          </div>
+      {children}
+    </body>
+    </html >
   );
 }
